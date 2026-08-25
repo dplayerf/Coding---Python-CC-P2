f7630be9d42b6184c6cb30718c29987f15d3c875
Daniel Mota <118938881+dplayerf@users.noreply.github.com>
2026-08-25 19:14:46 -0300

    Delete exercicio4.py

--- exercicio4.py
+++
@@ -1,6 +0,0 @@
-palavra = input("Digite uma palavra palíndromo: ")
-
-def eh_palindromo(texto):
-    texto_limpo = texto.replace(" ", "").lower()
-    return texto_limpo == texto_limpo[::-1]
-print(eh_palindromo(palavra))
