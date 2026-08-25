b52e85e7757bb18148cfbdc350ab9242da68fa66
Daniel Mota <118938881+dplayerf@users.noreply.github.com>
2026-08-25 19:13:58 -0300

    Delete exercicio1.py

--- exercicio1.py
+++
@@ -1,9 +0,0 @@
-print("=======Será que você pode votar?=======")
-
-nome = input("Digite um nome: ")
-idade = int(input("Digite um numero: "))
-
-if idade >= 16:
-    print(f"Você pode votar {nome}, vote com sabedoria!")
-else:
-    print(f"Você não pode votar ainda {nome}, espera so mais um pouco...")
